11eae108a14c515294f754a94f3c6483a1c7d445
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-07-13 10:26:23 +0200

    Add error reporting custom middleware.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,22 @@
-import express, {Express, Request, Response} from 'express';
+import express, {Express, NextFunction, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import { StatusCodes } from 'http-status-codes';
+import logging from "logging";
 
+let logger = logging('Application')
 dotenv.config()
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+   logger.error(err.message, err);
+   return res.status(StatusCodes.BAD_REQUEST).json({
+       error: err.message,
+   });
+});
 
 export default app;
