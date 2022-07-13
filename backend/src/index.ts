b609384c7ff339641a8e5126c5a5d71f4e93b902
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-07-13 12:53:04 +0200

    Import express application into entrypoint.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,24 @@
-import app from './server'
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+import {AppDataSource} from "./database";
+import logging from "logging";
+import {DatabaseLogger} from "./logger";
+import {stations} from "./models/stations";
+import {trips} from "./models/trips";
+import app from "./server";
+
+dotenv.config()
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
-});
+})
+
+AppDataSource.initialize()
+    .then(async () => {
+        DatabaseLogger.debug('Data source initialised correctly')
+    })
+    .catch((e) => {
+        DatabaseLogger.error(`Data source could not be initialised. ${e}`)
+    })
