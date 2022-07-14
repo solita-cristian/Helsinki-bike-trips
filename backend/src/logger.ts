2f884188dd4410c254f0eb3aa735d0b3d5804b78
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-07-14 11:39:51 +0200

    Add test logger for logging test related messages.

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -2,3 +2,4 @@ import logging from "logging";
 
 export const DatabaseLogger = logging('Database')
 export const ApplicationLogger = logging('Application')
+export const TestLogger = logging('Test')
