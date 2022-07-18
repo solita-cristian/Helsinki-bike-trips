029a65ea41ae940f307e9aec97598ef4d721bfa3
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-07-18 13:32:21 +0200

    When testing fetching a single station, also check if the values of the field of the error and of the successful response match.

--- backend/test/station/station.test.ts
+++ backend/test/station/station.test.ts
@@ -5,22 +5,33 @@ import '../base'
 import {IError} from '../../src/models/errors'
 
 describe("Station", () => {
-    const validUrl = '/stations/501'
-    const invalidUrl = '/stations/9999'
+    const validId = 501
+    const invalidId = 9999
+    const validUrl = `/stations/${validId}`
+    const invalidUrl = `/stations/${invalidId}`
 
     test("Should be returned when given valid ID", async () => {
-        const returnedStation = await request(await makeApp()).get(validUrl)
-        expect(returnedStation.statusCode).toBe(200)
-        expect(returnedStation.body).toBeTruthy()
+        const returnedStation = await request(await makeApp()).get(validUrl);
+        expect(returnedStation.statusCode).toBe(200);
+
+        // Check if the returned station is not null and has the correct ID
+        const station = returnedStation.body as stations;
+        expect(station).toBeTruthy();
+        expect(station.id).toEqual(validId)
     })
 
-    test("Should return an error message when station is not found", async () => {
-        const errorMessage = await request(await makeApp()).get(invalidUrl)
-        expect(errorMessage.statusCode).toBe(404)
-        const message: IError = errorMessage.body
-        expect(message).toBeTruthy()
-        expect(message.status).toEqual(errorMessage.statusCode)
-        expect(message.instance).toEqual(invalidUrl)
+    test("Should return a '404 not found' error message when is not found", async () => {
+        const errorMessage = await request(await makeApp()).get(invalidUrl);
+        expect(errorMessage.statusCode).toBe(404);
+
+        // Check if all the fields of the error message match
+        const message: IError = errorMessage.body;
+        expect(message).toBeTruthy();
+        expect(message.status).toEqual(errorMessage.statusCode);
+        expect(message.instance).toEqual(invalidUrl);
+        expect(message.id).toEqual('Not found');
+        expect(message.title).toEqual('Station not found');
+        expect(message.detail).toEqual(`The station with ID = ${invalidId} was not found`);
     })
 
 })
