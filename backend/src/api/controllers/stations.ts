bf2ab581113867b0d95267b531dcb87143d99b1c
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-07-18 16:52:46 +0200

    Add primitive station statistics retrieval

--- backend/src/api/controllers/stations.ts
+++ backend/src/api/controllers/stations.ts
@@ -113,137 +113,139 @@ export class StationsController extends BaseController<stations> {
         }
     }
 
-/**
- * Returns the average distance of the trips.
- * @param trips A list of trips
- */
-const getAverageDistance = (trips: trips[]) => {
-    return parseFloat((trips
+    /**
+     * Returns the average distance of the trips.
+     * @param trips A list of trips
+     */
+    private getAverageDistance = (trips: trips[]) => {
+        const sumDistances = (accumulatedTotal: number, currentDistance: number) => {
+            return accumulatedTotal + currentDistance;
+        }
+
+        return parseFloat((trips
                     .map(t => t.distance)
                     .reduce(sumDistances, 0)/trips.length)
                     .toFixed(2))
 }
 
-/**
- * Fetches the top 5 stations which start or finish at the requested station.
- *
- * @param outbound A boolean value indicating if the stations are the starting or ending point of the trip
- * @param month The month of the year. Used to filter the trips by month. If set to `null` will fetch all trips.
- * @param stationId The ID of the requested station
- */
-const getTopStations = async (outbound: boolean, month: number | null, stationId: string) => {
-    const station = (outbound ? 'return' : 'departure') + '_station';
-    const opp_station = (outbound ? 'departure' : 'return') + '_station';
+    /**
+     * Fetches the top 5 stations which start or finish at the requested station.
+     *
+     * @param outbound A boolean value indicating if the stations are the starting or ending point of the trip
+     * @param month The month of the year. Used to filter the trips by month. If set to `null` will fetch all trips.
+     * @param stationId The ID of the requested station
+     */
+    private getTopStations = async (outbound: boolean, month: number | null, stationId: string) => {
+        const station = (outbound ? 'return' : 'departure') + '_station';
+        const opp_station = (outbound ? 'departure' : 'return') + '_station';
+        const trips_repository = AppDataSource.getRepository('trips')
 
-    return (await trips_repository
-        .createQueryBuilder('getTopStations')
-        .select(`${station}, COUNT(*) as total`)
-        .where(`${month ? `extract(MONTH from departure_time) = :month and` : ''} ${opp_station} = :stationId`,
-            {month: month, stationId: parseInt(stationId)})
-        .groupBy(`${station}`)
-        .orderBy('total', 'DESC')
-        .cache(true)
-        .getRawMany<stations>()).slice(0, 5);
-}
+        return (await trips_repository
+            .createQueryBuilder('getTopStations')
+            .select(`${station}, COUNT(*) as total`)
+            .where(`${month ? `extract(MONTH from departure_time) = :month and` : ''} ${opp_station} = :stationId`,
+                {month: month, stationId: parseInt(stationId)})
+            .groupBy(`${station}`)
+            .orderBy('total', 'DESC')
+            .cache(true)
+            .getRawMany<stations>()).slice(0, 5);
+    }
 
-/**
- * Encapsulated a mapping function which will generate all the statistics for a station.
- *
- * @param outbound_trips The station's outbound trips
- * @param inbound_trips The station's inbound trips
- * @param stationId The ID of the station
- */
-const calculateStatisticsPerMonth = (outbound_trips: trips[], inbound_trips: trips[], stationId: string) =>
-    async (month: number) => {
-        const outbound = outbound_trips.filter(t => t.departure_time.getMonth() === month);
-        const inbound = inbound_trips.filter(t => t.departure_time.getMonth() === month);
-        const average_distance_inbound = getAverageDistance(inbound)
-        const average_distance_outbound = getAverageDistance(outbound)
+    /**
+     * Encapsulated a mapping function which will generate all the statistics for a station.
+     *
+     * @param outbound_trips The station's outbound trips
+     * @param inbound_trips The station's inbound trips
+     * @param stationId The ID of the station
+     */
+    private calculateStatisticsPerMonth = (outbound_trips: trips[], inbound_trips: trips[], stationId: string) =>
+        async (month: number) => {
+            const outbound = outbound_trips.filter(t => t.departure_time.getMonth() === month);
+            const inbound = inbound_trips.filter(t => t.departure_time.getMonth() === month);
+            const average_distance_inbound = this.getAverageDistance(inbound)
+            const average_distance_outbound = this.getAverageDistance(outbound)
 
-        return {
-            total_inbound: inbound.length,
-            total_outbound: outbound.length,
-            average_distance_inbound,
-            average_distance_outbound,
-            top_outbound: await getTopStations(true, month + 1, stationId),
-            top_inbound: await getTopStations(false, month + 1, stationId)
+            return {
+                total_inbound: inbound.length,
+                total_outbound: outbound.length,
+                average_distance_inbound,
+                average_distance_outbound,
+                top_outbound: await this.getTopStations(true, month + 1, stationId),
+                top_inbound: await this.getTopStations(false, month + 1, stationId)
+            }
+        }
+
+    /**
+     * Returns the statistics concerning a station, given its ID, divided pe month, as well as considering the whole.
+     *
+     * The statistics are:
+     *
+     * - Total number of trips starting from the station
+     * - Total number of trips ending at the station
+     * - The average distance of a trip starting from the station
+     * - The average distance of a trip ending at the station
+     * - Top 5 most popular return stations for trips starting from the station
+     * - Top 5 most popular departure stations for journeys ending at the station
+     */
+    getStatistics = () => {
+        return async (req: Request, res: Response) => {
+            const {stationId} = req.params;
+            const month = req.query.month
+
+            if (month) {
+                let m = parseInt(month as any)
+                if(m < 1 || m > 12)
+                    return this.badParameterError(req, res, 'month', m, '1 <= month <= 12')
+            }
+
+            const station = await this.getStationById(stationId)
+
+            let outbound_trips = await (station as stations).outbound_trips;
+            let inbound_trips = await (station as stations).inbound_trips;
+
+            const average_distance_inbound = this.getAverageDistance(inbound_trips)
+            const average_distance_outbound = this.getAverageDistance(outbound_trips)
+
+            /**
+             * If `month` is set, then calculate the statistics only for that month, otherwise calculate the statistics
+             * for the whole year.
+             */
+            const stats = await Promise.all(
+                (month ?
+                    [parseInt(month as any) - 1] :
+                    [...Array(12).keys()])
+                .map(this.calculateStatisticsPerMonth(outbound_trips, inbound_trips, stationId))
+            )
+
+            /**
+             * If `month` is not set, to the calculation per month, add the totals, otherwise just send the result of
+             * that particular month.
+             */
+            res.status(200).json(
+                !month ? { total : {
+                    total_inbound: inbound_trips.length,
+                    total_outbound: outbound_trips.length,
+                    average_distance_inbound,
+                    average_distance_outbound,
+                    top_outbound: await this.getTopStations(true, null, stationId),
+                    top_inbound: await this.getTopStations(false, null, stationId)
+                },
+                monthly: [...stats]} :
+                stats
+            )
+
         }
     }
 
+}
 
 /**
- * Returns the statistics concerning a station, given its ID, divided pe month, as well as considering the whole.
- *
- * The statistics are:
- *
- * - Total number of trips starting from the station
- * - Total number of trips ending at the station
- * - The average distance of a trip starting from the station
- * - The average distance of a trip ending at the station
- * - Top 5 most popular return stations for trips starting from the station
- * - Top 5 most popular departure stations for journeys ending at the station
- *
- *
- * @param req The request
- * @param res The response
+ * This is a workaround for swagger-express, because it needs that the name of the method is exactly the same that's
+ * written in the `operationId` field in the .yaml files.
  */
-export const getStationStatistics = async (req: Request, res: Response) => {
-    const {stationId} = req.params;
-    const month = req.query.month
+const stationsController = new StationsController()
 
-    if (month) {
-        let m = parseInt(month as any)
-        if(m < 1 || m > 12)
-            return res.status(400).json(buildError(
-                "Bad parameter",
-                "A badly formatted parameter was found",
-                400,
-                `The parameter month has value ${m}. Expected between [1, 12].`,
-                req.url
-            ))
-    }
-
-    const station = await stations_repository
-        .createQueryBuilder()
-        .where('stations.id = :id', {id: stationId})
-        .cache(true)
-        .getOne();
-
-    if (!station)
-        return res.status(404).json(buildError(
-            "Not found",
-            "Station not found",
-            404,
-            `The station with ID ${stationId} was not found`,
-            req.url
-        ))
-
-
-    let outbound_trips = await (station as stations).outbound_trips;
-    let inbound_trips = await (station as stations).inbound_trips;
-
-    const average_distance_inbound = getAverageDistance(inbound_trips)
-    const average_distance_outbound = getAverageDistance(outbound_trips)
-
-    const stats = await Promise.all(
-        (month ?
-            [parseInt(month as any) - 1] :
-            [...Array(12).keys()]
-        ).map(
-            calculateStatisticsPerMonth(outbound_trips, inbound_trips, stationId)
-    ))
-
-    res.status(200).json(
-        !month ? { total : {
-            total_inbound: inbound_trips.length,
-            total_outbound: outbound_trips.length,
-            average_distance_inbound,
-            average_distance_outbound,
-            top_outbound: await getTopStations(true, null, stationId),
-            top_inbound: await getTopStations(false, null, stationId)
-        },
-        monthly: [...stats]} :
-        stats
-    )
-
-}
+export const getAllStations = stationsController.getStations()
+export const getStation = stationsController.getStation()
+//export const searchStations = stationsController.getStations()
+export const getStationStatistics = stationsController.getStatistics()
