353d80b3f260249b329132ed6a839104cbb38446
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-07-18 14:00:52 +0200

    Refactor error types for better clarity and modularity.

--- backend/src/models/errors.ts
+++ backend/src/models/errors.ts
@@ -6,13 +6,40 @@ export interface IError {
     instance: string
 }
 
-export const buildError = (id: string, title: string, status: number, detail: string, url: string) => {
-    const error: IError = {
-        id: id,
-        title: title,
-        status: status,
-        detail: detail,
-        instance: url
-    };
-    return error;
+export abstract class Error implements IError {
+    public id: string;
+    public title: string;
+    public status: number;
+    public detail: string;
+    public instance: string;
+
+    protected constructor(id: string, title: string, status: number, detail: string, instance: string) {
+        this.id = id;
+        this.title = title;
+        this.status = status;
+        this.detail = detail;
+        this.instance = instance;
+    }
+
+    build = () => {
+        return {
+            id: this.id,
+            title: this.title,
+            status: this.status,
+            detail: this.detail,
+            instance: this.instance
+        };
+    }
+}
+
+export class NotFoundError extends Error {
+    constructor(title: string, detail: string, instance: string) {
+        super('Not found', title, 404, detail, instance)
+    }
+}
+
+export class BadParameterError extends Error {
+    constructor(title: string, detail: string, instance: string) {
+        super('Badly formatted parameter', title, 400, detail, instance)
+    }
 }
