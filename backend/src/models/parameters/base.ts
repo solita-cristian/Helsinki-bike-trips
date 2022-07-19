a3590ecbde84cc009f608514ddf6a495b2100e8b
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-07-19 20:53:23 +0200

    Change page and perPage to nullable types

--- backend/src/models/parameters/base.ts
+++ backend/src/models/parameters/base.ts
@@ -5,7 +5,7 @@ export abstract class BaseParameters {
     page?: number;
     perPage?: number;
 
-    protected constructor(page: number, perPage: number) {
+    protected constructor(page?: number, perPage?: number) {
         this.page = page;
         this.perPage = perPage;
     }
