e7e0d4ec34a3894cde28db0dd70c1b6c947feac1
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-08-02 11:00:33 +0200

    Create station and station page model

---
+++ frontend/src/models/Stations.tsx
@@ -0,0 +1,20 @@
+export type Station = {
+    id: number,
+    name_fi: string,
+    name_se: string,
+    name_en: string,
+    address_fi: string,
+    address_se: string,
+    city_fi?: string,
+    city_se?: string,
+    operator?: string,
+    capacity: number,
+    x: number,
+    y: number
+}
+
+export type StationPage = {
+    data: Station[],
+    page: number,
+    perPage: number
+}
