cb0794618b83e6b2f1fb9acc3da2f2328733e382
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2022-08-05 09:37:10 +0200

    Filter form now has its own internal state, and will change the parameters only when a button is pressed, to avoid re-renders and focus loss on the form

--- frontend/src/components/stations/FilterForm.tsx
+++ frontend/src/components/stations/FilterForm.tsx
@@ -1,5 +1,5 @@
 import { Paper, Grid, Typography, Button } from '@mui/material'
-import { ChangeEvent } from 'react'
+import { ChangeEvent, Fragment, useState } from 'react'
 import { StationsParams } from '../../models/Params'
 import ClearIcon from '@mui/icons-material/Clear'
 import AdornedTextField from '../Forms/AdornedTextField'
@@ -7,6 +7,7 @@ import BadgeIcon from '@mui/icons-material/Badge';
 import HomeIcon from '@mui/icons-material/Home';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
 import BusinessIcon from '@mui/icons-material/Business';
+import NumbersIcon from '@mui/icons-material/Numbers';
 
 interface FilterFormProps {
     params: Partial<StationsParams>
@@ -15,69 +16,83 @@ interface FilterFormProps {
 }
 
 export default function FilterForm({params, updateParams, clearParams}: FilterFormProps) {
+
+    const [state, setState] = useState(params)
+
+    const handleState = (newParams: Partial<StationsParams>) => {
+        setState({...state, ...newParams})
+    }
+
   return (
-    <Paper>
+    <div style={{alignItems: 'center', justifyContent: 'center'}}>
         <Typography variant='h6' align='center'>
             Search stations
         </Typography>
-        <Grid container>
-            <Grid item xs={12}>
-                <Button variant='outlined' startIcon={<ClearIcon />} onClick={clearParams}>
-                    Clear
-                </Button>
-            </Grid>
-            <Grid item xs={4}>
+        <Grid container sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 2}}>
+            <Grid item>
                 <AdornedTextField 
                     id='name'
                     label='Name'
-                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {updateParams({name: e.target.value})}}
+                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {handleState({name: e.target.value})}}
                     adornmentImage={<BadgeIcon />}
                     type='text'
-                    value={params.name}
+                    value={state.name}
                 />
             </Grid>
-            <Grid item xs={4}>
+            <Grid item>
                 <AdornedTextField 
                     id='address'
                     label='Address'
-                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {updateParams({address: e.target.value})}}
+                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {handleState({address: e.target.value})}}
                     adornmentImage={<HomeIcon />}
                     type='text'
-                    value={params.address}
+                    value={state.address}
                 />
             </Grid>
-            <Grid item xs={4}>
+            <Grid item>
                 <AdornedTextField 
                     id='city'
                     label='City'
-                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {updateParams({city: e.target.value})}}
+                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {handleState({city: e.target.value})}}
                     adornmentImage={<LocationCityIcon />}
                     type='text'
-                    value={params.city}
+                    value={state.city}
                 />
             </Grid>
-            <Grid item xs={6}>
+            <Grid item>
                 <AdornedTextField 
                     id='operator'
                     label='Operator'
-                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {updateParams({operator: e.target.value})}}
+                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {handleState({operator: e.target.value})}}
                     adornmentImage={<BusinessIcon />}
                     type='text'
-                    value={params.operator}
+                    value={state.operator}
                 />
             </Grid>
-            <Grid item xs={6}>
+            <Grid item>
                 <AdornedTextField 
                     id='capacity'
                     label='Capacity'
-                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {updateParams({capacity: parseInt(e.target.value)})}}
-                    adornmentImage={<BusinessIcon />}
+                    onTextChange={(e: ChangeEvent<HTMLInputElement>) => {handleState({capacity: parseInt(e.target.value)})}}
+                    adornmentImage={<NumbersIcon />}
                     type='number'
-                    value={params.capacity}
+                    value={state.capacity}
                 />
             </Grid>
         </Grid>
-    </Paper>
+        <Grid container spacing={1} sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 2, mb: 1}}>
+        <Grid item>
+                <Button variant='contained' startIcon={<ClearIcon />} onClick={clearParams} size='small' color='error'>
+                    Clear
+                </Button>
+            </Grid>
+            <Grid item>
+                <Button variant='contained' onClick={() => {updateParams(state)}} size='small' color='primary'>
+                    Filter
+                </Button>
+            </Grid>
+        </Grid>
+    </div>
     
   )
 }
